be312fa915e5de060844890de514168e4af19808
Vishvara <142617173+vishvara-sharda@users.noreply.github.com>
2026-08-24 12:32:30 +0530

    Remove Analytics component from main render

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,12 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import { Analytics } from "@vercel/analytics/react";
+
 import App from "./app/App.tsx";
 import "./styles/index.css";
 
 createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
-    <Analytics />
+
   </React.StrictMode>
 );
